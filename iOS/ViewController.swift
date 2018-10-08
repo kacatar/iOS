41a429cb286bea662a5ff15bef1645fc39af2ecf
Kacatar <helminizer@gmail.com>
2018-10-08 11:55:32 +0700

    First Commit

---
+++ iOS/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  iOS
+//
+//  Created by Alvin Wandana on 05/10/18.
+//  Copyright © 2018 Alvin Wandana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    func printword() {
+        print("a")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
